b1c1012600b7294860fb58bfaffc7e2f0a462d8f
Valdera <f.valdera@yahoo.co.id>
2023-09-19 19:04:40 +0700

    feat: add scss and tailwind support

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const path = require('path');
+
+const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+    sassOptions: {
+        includePaths: [
+            path.join(__dirname, 'styles'),
+            path.join(__dirname, 'components'),
+        ],
+    }
+}
 
 module.exports = nextConfig
